e57f9ef3b71d8f745731b0655fd6b059e226f385
NykolaR <thenykreed@gmail.com>
2018-01-27 16:08:47 -0600

    come together

--- player.gd
+++ player.gd
@@ -15,44 +15,45 @@ func _ready():
 	pass
 
 func _physics_process(delta):
-	var movement = Vector3(0, 0, 0)
-	
-	if Input.is_action_pressed("up"):
-	    movement.z -= 1
-	if Input.is_action_pressed("down"):
-	    movement.z += 1
-	if Input.is_action_pressed("right"):
-	    movement.x += 1
-	if Input.is_action_pressed("left"):
-	    movement.x -= 1
-	
-	movement = movement.rotated(Vector3(0, 1, 0), $cam_y.rotation.y)
-	movement *= movement_speed
-	vspeed -= gravity
-	movement.y = vspeed
-	
-	move_and_slide(movement, Vector3(0, 1, 0))
-	
-	if is_on_floor():
-	    vspeed = 0
-	    airJumpsRemaining = AIR_JUMPS
-	    
-	    if Input.is_action_just_pressed("jump"):
-	    	vspeed = jump
-	else:
-	    if Input.is_action_just_pressed("jump"):
-	    	if  (airJumpsRemaining):
-	    	    vspeed = jump / 1.40
-	    	    airJumpsRemaining -= 1
-	
-	if orb.state == orb.ATTACHED:
-		orb.translation = translation + Vector3(0, 0.2, -0.2).rotated(Vector3(0, 1, 0), $cam_y.rotation.y)
-	
-	if Input.is_action_just_pressed("throw"):
-		#var d = Vector3(-$cam_y.rotation.y, $cam_y/cam_x.rotation.x, 0)
-		#var d = Vector3(0, $cam_y/cam_x.rotation.x, 0)
-		#print(d)
-		orb._set_direction(Vector3(-sin($cam_y.rotation.y), $cam_y/cam_x.rotation.x, -cos($cam_y.rotation.y)))
+	if is_network_master():
+		var movement = Vector3(0, 0, 0)
+		
+		if Input.is_action_pressed("up"):
+		    movement.z -= 1
+		if Input.is_action_pressed("down"):
+		    movement.z += 1
+		if Input.is_action_pressed("right"):
+		    movement.x += 1
+		if Input.is_action_pressed("left"):
+		    movement.x -= 1
+		
+		movement = movement.rotated(Vector3(0, 1, 0), $cam_y.rotation.y)
+		movement *= movement_speed
+		vspeed -= gravity
+		movement.y = vspeed
+		
+		move_and_slide(movement, Vector3(0, 1, 0))
+		
+		if is_on_floor():
+		    vspeed = 0
+		    airJumpsRemaining = AIR_JUMPS
+		    
+		    if Input.is_action_just_pressed("jump"):
+		    	vspeed = jump
+		else:
+		    if Input.is_action_just_pressed("jump"):
+		    	if  (airJumpsRemaining):
+		    	    vspeed = jump / 1.40
+		    	    airJumpsRemaining -= 1
+		
+		if orb.state == orb.ATTACHED:
+			orb.translation = translation + Vector3(0, 0.2, -0.2).rotated(Vector3(0, 1, 0), $cam_y.rotation.y)
+		
+		if Input.is_action_just_pressed("throw"):
+			#var d = Vector3(-$cam_y.rotation.y, $cam_y/cam_x.rotation.x, 0)
+			#var d = Vector3(0, $cam_y/cam_x.rotation.x, 0)
+			#print(d)
+			orb.rpc("_set_direction", Vector3(-sin($cam_y.rotation.y), $cam_y/cam_x.rotation.x, -cos($cam_y.rotation.y)))
 
 func _input(event):
 	if event is InputEventMouseMotion:
