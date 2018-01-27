dd79d974bfff4a7b2d8134db1f91c9a6090518cf
NykolaR <thenykreed@gmail.com>
2018-01-27 11:34:37 -0600

    player movement

--- player.gd
+++ player.gd
@@ -1,17 +1,45 @@
 extends KinematicBody
 
 var sensitivity = Vector2(-0.01, -0.003)
+var vspeed = 0
+var gravity = 0.01
+const jump = 5
+var movement_speed = 5
 
 func _ready():
 	pass
 
 func _physics_process(delta):
-	pass
+	var movement = Vector3(0, 0, 0)
+	
+	if Input.is_action_pressed("up"):
+		movement.z -= 1
+	if Input.is_action_pressed("down"):
+		movement.z += 1
+	if Input.is_action_pressed("right"):
+		movement.x += 1
+	if Input.is_action_pressed("left"):
+		movement.x -= 1
+	
+	movement = movement.rotated(Vector3(0, 1, 0), $cam_y.rotation.y)
+	movement *= movement_speed
+	vspeed -= gravity
+	movement.y = vspeed
+	
+	move_and_slide(movement)
+	
+	if is_on_floor():
+		vspeed = 0
+		print("floor")
+		
+		if Input.is_action_just_pressed("jump"):
+			vspeed = jump
 
 func _input(event):
 	if event is InputEventMouseMotion:
 		$cam_y.rotate_y(event.relative.x * sensitivity.x)
 		$cam_y/cam_x.rotate_x(event.relative.y * sensitivity.y)
-		$cam_y/cam_x.rotation.x = clamp($cam_y/cam_x.rotation.x, -0.5, 0.5)
+		$cam_y/cam_x.rotation.x = clamp($cam_y/cam_x.rotation.x, -1, 1)
+		
 		#print($cam_y/cam_x.rotation)
 		#print(event.relative)
