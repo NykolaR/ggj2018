e2a371e441833d7bf3b730f63c975333a6a803c3
NykolaR <thenykreed@gmail.com>
2018-01-28 13:33:50 -0600

    f

--- scenes/management/game.gd
+++ scenes/management/game.gd
@@ -9,7 +9,8 @@ func _ready():
 		$player2.set_network_master(get_tree().get_network_connected_peers()[0])
 		$player1/player/cam_y/cam_x/camera.current = true
 		
-		rpc("spawn_level", "res://scenes/level_scenes/intro_level_2.tscn", $init_spawn.translation)
+		#rpc("spawn_level", "res://scenes/level_scenes/intro_level_2.tscn", $init_spawn.translation)
+		#rpc("spawn_level", "res://scenes/level_scenes/levels_spawn/level4.tscn", $init_spawn.translation)
 	elif not get_tree().network_peer == null:
 		# else, give control to self (recursive)
 		$player2.set_network_master(get_tree().get_network_unique_id())
@@ -24,14 +25,14 @@ func _input(event):
 	if event.is_action_pressed("exit_game"):
 		emit_signal("game_finished")
 
-sync func spawn_level(level_path, transl):
-	# spawn level
-	var level = load(level_path).instance()
-	level.translation = transl
-	
-	level.get_node("level_end").connect("spawn_next_level", self, "spawn_level")
-	
-	$level_holder.add_child(level)
-	
-	if $level_holder.get_child_count() > 4:
-		$level_holder.get_child(0).queue_free()
+#sync func spawn_level(level_path, transl):
+#	# spawn level
+#	var level = load(level_path).instance()
+#	level.translation = transl
+#	
+#	level.get_node("level_end").connect("spawn_next_level", self, "spawn_level")
+#	
+#	$level_holder.add_child(level)
+#	
+#	if $level_holder.get_child_count() > 4:
+#		$level_holder.get_child(0).queue_free()
